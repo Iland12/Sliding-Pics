dbc4b5e85d1647f34968a5460ad015d28d1ad84e
Iland <iland12maimom@gmail.com>
2025-09-07 23:08:41 +0530

    Updated

--- engine.js
+++ engine.js
@@ -1,8 +1,11 @@
 const img_container = document.querySelector("ul");
 const heading = document.querySelector("h1");
 const cl = document.querySelector("button");
+const audio = document.querySelector("audio");
 
 cl.addEventListener("click", () => {
   heading.style.display = "block";
   img_container.style.display = "flex";
+  audio.autoplay = true;
+  audio.play();
 });
